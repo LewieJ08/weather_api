1b85331f84546fd5655df904db2490730ae11ece
LewieJ08 <lewiej08@gmail.com>
2025-04-12 14:55:00 +0100

    app routing logic

--- weather_api/app.py
+++ weather_api/app.py
@@ -1,8 +1,10 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request
+from flask_limiter import Limiter
 from weather import fetch_data
-from cache import cache
+from cache import cache, fetch
 from dotenv import load_dotenv
 import os
+import json
 
 load_dotenv()
 
@@ -15,10 +17,15 @@ def index():
     api_key = os.getenv("API_KEY")
     redis_url = os.getenv("REDIS_URL")
 
-    data = fetch_data(location, api_key)
-    cache(data, redis_url)
+    data = fetch(location, redis_url)
+    if data != None:
+        return data
+    else:
+        data = fetch_data(location, api_key)
+        cache_data = json.dumps(data,indent = 4)
+        cache(location, cache_data, redis_url)
   
-    return jsonify(data)
+    return data
 
 if __name__ == "__main__":
     app.run(debug=True)
