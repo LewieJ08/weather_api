828faf22139c84405190d57b6b2a5bb6023fa810
LewieJ08 <lewiej08@gmail.com>
2025-04-12 23:20:27 +0100

    update setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="weather_api",
+    version="1.0.0",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "Flask",
+        "Flask-Limiter",
+        "requests",
+        "redis",
+        "python-dotenv"
+    ],  
+    entry_points={  
+        "console_scripts": [
+            "weather-api=weather_api.app:app.run" 
+        ]
+    },
+    author="Lewie Jackson",
+    author_email="LewieJ08@gmail.com",
+    description="A simple weather api built with flask that uses redis for caching",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/LewieJ08/weather_api",  
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
