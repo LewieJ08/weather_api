c26fd124a8013a8420fa220d7a2d44de8b10ec77
LewieJ08 <lewiej08@gmail.com>
2025-04-12 22:32:19 +0100

    flask limiter, rate limiting

--- weather_api/app.py
+++ weather_api/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 from weather import fetch_data
 from cache import cache, fetch
 from dotenv import load_dotenv
@@ -10,7 +11,14 @@ load_dotenv()
 
 app = Flask(__name__)
 
+limiter = Limiter(
+    app=app,
+    key_func=get_remote_address,
+    storage_uri="memory://"
+)
+
 @app.route("/", methods = ["GET"])
+@limiter.limit("10 per hour")
 
 def index():
     location = request.args.get("location")
@@ -24,13 +32,14 @@ def index():
     
     if data != None:
         data = json.loads(data)
+        print("Data fetched")
         return jsonify(data)
     
     else:
         data = fetch_data(location, api_key)
         cache_data = json.dumps(data)
         cache(location, cache_data, redis_url)
-  
+
     return jsonify(data)
 
 if __name__ == "__main__":
